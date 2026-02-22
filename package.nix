d3712d47a5202162b20f5a8a5086bf8dc25b6c83
Evgeny Zislis <evgeny.zislis@gmail.com>
2026-02-22 17:45:45 +0200

    Add ldflags for version injection
    Use ldflags to inject version at build time, following standard Go
    nixpkgs practice. This allows the binary to report its version.

--- package.nix
+++ package.nix
@@ -6,10 +6,18 @@
   pkg-config,
 }:
 
+let
+  version = "0.1.0";
+in
 buildGoModule {
   inherit src vendorHash;
   pname = "kiro-go-gw";
-  version = "0.1.0";
+  inherit version;
+
+  ldflags = [
+    "-X main.version=${version}"
+  ];
+
   subPackages = [ "cmd/server" ];
   buildInputs = [ sqlite ];
   nativeBuildInputs = [ pkg-config ];
